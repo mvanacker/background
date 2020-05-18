3b707c6db83d37d40b35c3fcde535a9793f1a62d
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-18 15:18:15 +0200

    Made navigation sticky.

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function Navigation(props) {
   const { items, titles, paths, level } = props;
-  let navColor = level ? `w3-theme-d${5 - level}` : 'w3-theme-d4';
-  const navStyle = `w3-bar ${navColor}`;
+  const navTheme = level ? `w3-theme-d${5 - level}` : 'w3-theme-d4';
+  const navStyle = `my-nav w3-bar ${navTheme}`;
   const linkStyle = 'w3-bar-item w3-button w3-hover-theme';
-  return <nav className={navStyle}>
+  const offset = { top: `${38 * (level - 1)}px` };
+  return <nav className={navStyle} style={offset}>
     {
       Object.entries(items).map(([key, val]) => 
         <Link key={val} className={linkStyle} to={paths[key]}>
