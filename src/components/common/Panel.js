9a02f43900f8f9b997857ff525376fafdab853b0
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-18 16:15:49 +0200

    Set Oswald as default title font.

--- src/components/common/Panel.js
+++ src/components/common/Panel.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export default function Panel(props) {
-  const { theme, title, children, padding, margin } = props;
+  const { theme, title, children, padding, margin, titleFont } = props;
   const _theme = !theme ? 'w3-theme-d3' : theme;
   const _padding = padding ? 'w3-padding-16' : '';
   const _margin = margin === undefined || margin === true ? 'w3-margin' : '';
+  const titleStyle = { fontFamily: titleFont ? titleFont : 'Oswald' };
   return <div className={`${_margin} w3-container w3-padding-16 ${_theme}`}>
     {
       title === false ? ''
-        : <h4 className="w3-center">{title}</h4>
+        : <h4 className="w3-center" style={titleStyle}>{title}</h4>
     }
     <div className={_padding}>{children}</div>
   </div>;
