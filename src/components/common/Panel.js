e775e5a5d522b13710cab733bbe637453f031128
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-18 12:29:43 +0200

    Made title optional + corrected idiom.

--- src/components/common/Panel.js
+++ src/components/common/Panel.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export default function Panel(props) {
-  let { theme, title, children, padding, margin } = props;
-  theme = !theme ? 'w3-theme-d3' : theme;
-  padding = padding ? 'w3-padding-16' : '';
-  margin = margin === undefined || margin === true ? 'w3-margin' : '';
-  return <div className={`${margin} w3-container w3-padding-16 ${theme}`}>
-    <h4 className="w3-center">{title}</h4>
-    <div className={padding}>{children}</div>
+  const { theme, title, children, padding, margin } = props;
+  const _theme = !theme ? 'w3-theme-d3' : theme;
+  const _padding = padding ? 'w3-padding-16' : '';
+  const _margin = margin === undefined || margin === true ? 'w3-margin' : '';
+  return <div className={`${_margin} w3-container w3-padding-16 ${_theme}`}>
+    {
+      title === false ? ''
+        : <h4 className="w3-center">{title}</h4>
+    }
+    <div className={_padding}>{children}</div>
   </div>;
 }
