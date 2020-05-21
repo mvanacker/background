0d6ff8d14c7bd263e71225072fda94b8a2fc16a2
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-21 22:21:19 +0200

    Added future to spot calculator. Reorganized lay-out.

--- src/components/easter-eggs/Futures.js
+++ src/components/easter-eggs/Futures.js
@@ -7,7 +7,8 @@ const daysId   = 'days';
 const hoursId  = 'hours';
 const minsId   = 'mins';
 const annualId = 'annual';
-const targetId = 'target';
+const spotTargetId = 'spot-target';
+const futureTargetId = 'future-target';
 
 const get = (id)      => localStorage.getItem(id);
 const set = (id, val) => localStorage.setItem(id, val);
@@ -21,69 +22,102 @@ const Field = ({ id, value, setValue }) => <input
 />;
 
 const Row = ({ left, right }) => <div className="w3-cell-row">
-  <div className="w3-cell" style={{width: '140px'}}>{left}</div>
+  <div className="w3-cell" style={{width: '160px'}}>{left}</div>
   <div className="w3-cell">{right}</div>
 </div>;
 
 export default function Futures() {
-  const [future, setFuture] = useState(get(futureId) ? get(futureId) : NaN);
-  const [spot,   setSpot]   = useState(get(spotId)   ? get(spotId)   : NaN);
-  const [days,   setDays]   = useState(get(daysId)   ? get(daysId)   : NaN);
-  const [hours,  setHours]  = useState(get(hoursId)  ? get(hoursId)  : NaN);
-  const [mins,   setMins]   = useState(get(minsId)   ? get(minsId)   : NaN);
-  const [annual, setAnnual] = useState(get(annualId) ? get(annualId) : 365);
-  const [target, setTarget] = useState(get(targetId) ? get(targetId) : NaN);
+  const [future,       setFuture]       = useState(get(futureId)       ? get(futureId)       : NaN);
+  const [spot,         setSpot]         = useState(get(spotId)         ? get(spotId)         : NaN);
+  const [days,         setDays]         = useState(get(daysId)         ? get(daysId)         : NaN);
+  const [hours,        setHours]        = useState(get(hoursId)        ? get(hoursId)        : NaN);
+  const [mins,         setMins]         = useState(get(minsId)         ? get(minsId)         : NaN);
+  const [annual,       setAnnual]       = useState(get(annualId)       ? get(annualId)       : 365);
+  const [spotTarget,   setSpotTarget]   = useState(get(spotTargetId)   ? get(spotTargetId)   : NaN);
+  const [futureTarget, setFutureTarget] = useState(get(futureTargetId) ? get(futureTargetId) : NaN);
 
-  useEffect(() => set(futureId, future), [future]);
-  useEffect(() => set(spotId,   spot),   [spot]);
-  useEffect(() => set(daysId,   days),   [days]);
-  useEffect(() => set(hoursId,  hours),  [hours]);
-  useEffect(() => set(minsId,   mins),   [mins]);
-  useEffect(() => set(annualId, annual), [annual]);
-  useEffect(() => set(targetId, target), [target]);
+  useEffect(() => set(futureId,       future),       [future]);
+  useEffect(() => set(spotId,         spot),         [spot]);
+  useEffect(() => set(daysId,         days),         [days]);
+  useEffect(() => set(hoursId,        hours),        [hours]);
+  useEffect(() => set(minsId,         mins),         [mins]);
+  useEffect(() => set(annualId,       annual),       [annual]);
+  useEffect(() => set(spotTargetId,   spotTarget),   [spotTarget]);
+  useEffect(() => set(futureTargetId, futureTarget), [futureTarget]);
 
-  const [ffuture, fspot, fannual, fdays, fhours, fmins, ftarget]
-    = [future, spot, annual, days, hours, mins, target].map(parseFloat);
+  const [
+    ffuture, fspot, fannual, fdays, fhours, fmins, fspotTarget, ffutureTarget
+  ] = [
+    future, spot, annual, days, hours, mins, spotTarget, futureTarget
+  ].map(parseFloat);
   const premium = (ffuture / fspot - 1) * 1440 * fannual
                   / (1440 * fdays + 60 * fhours + fmins);
-  const equivalent = ftarget * ffuture / fspot;
+  const future_equiv = fspotTarget * ffuture / fspot;
+  const spot_equiv = ffutureTarget * fspot / ffuture;
 
-  return <form className="w3-container w3-section" style={{width: '275px'}}>
-    <Row
-      left={<label htmlFor={futureId}>Future price</label>}
-      right={<Field id={futureId} value={future} setValue={setFuture}/>}
-    />
-    <Row
-      left={<label htmlFor={spotId}>Spot price</label>}
-      right={<Field id={spotId} value={spot} setValue={setSpot}/>}
-    />
-    <Row
-      left={<label htmlFor={daysId}>Days remaining</label>}
-      right={<Field id={daysId} value={days} setValue={setDays}/>}
-    />
-    <Row
-      left={<label htmlFor={hoursId}>Hours remaining</label>}
-      right={<Field id={hoursId} value={hours} setValue={setHours}/>}
-    />
-    <Row
-      left={<label htmlFor={minsId}>Mins remaining</label>}
-      right={<Field id={minsId} value={mins} setValue={setMins}/>}
-    />
-    <Row
-      left={<label htmlFor={annualId}>Annual</label>}
-      right={<Field id={annualId} value={annual} setValue={setAnnual}/>}
-    />
-    <Row
-      left="Premium"
-      right={isNaN(premium) ? 'n/a' : `${round_to(100 * premium, 1)}%`}
-    />
-    <Row
-      left={<label htmlFor={targetId}>Spot target</label>}
-      right={<Field id={targetId} value={target} setValue={setTarget}/>}
-    />
-    <Row
-      left="Future equivalent"
-      right={isNaN(equivalent) ? 'n/a' : `${Math.round(equivalent)}`}
-    />
+  return <form className="w3-container" style={{width: '275px'}}>
+    <div className="w3-section">
+      <h1>Input</h1>
+      <Row
+        left={<label htmlFor={futureId}>Future price</label>}
+        right={<Field id={futureId} value={future} setValue={setFuture}/>}
+      />
+      <Row
+        left={<label htmlFor={spotId}>Spot price</label>}
+        right={<Field id={spotId} value={spot} setValue={setSpot}/>}
+      />
+    </div>
+    
+    <div className="w3-section">
+      <h1>Spot to future</h1>
+      <Row
+        left={<label htmlFor={spotTargetId}>Spot target</label>}
+        right={<Field
+          id={spotTargetId} value={spotTarget} setValue={setSpotTarget}
+        />}
+      />
+      <Row
+        left="Future equivalent"
+        right={isNaN(future_equiv) ? 'n/a' : `${Math.round(future_equiv)}`}
+      />
+    </div>
+
+    <div className="w3-section">
+      <h1>Future to spot</h1>
+      <Row
+        left={<label htmlFor={futureTargetId}>Future target</label>}
+        right={<Field
+          id={futureTargetId} value={futureTarget} setValue={setFutureTarget}
+        />}
+      />
+      <Row
+        left="Spot equivalent"
+        right={isNaN(spot_equiv) ? 'n/a' : `${Math.round(spot_equiv)}`}
+      />
+    </div>
+
+    <div className="w3-section">
+      <h1>Premium</h1>
+      <Row
+        left={<label htmlFor={daysId}>Days remaining</label>}
+        right={<Field id={daysId} value={days} setValue={setDays}/>}
+      />
+      <Row
+        left={<label htmlFor={hoursId}>Hours remaining</label>}
+        right={<Field id={hoursId} value={hours} setValue={setHours}/>}
+      />
+      <Row
+        left={<label htmlFor={minsId}>Mins remaining</label>}
+        right={<Field id={minsId} value={mins} setValue={setMins}/>}
+      />
+      <Row
+        left={<label htmlFor={annualId}>Annual</label>}
+        right={<Field id={annualId} value={annual} setValue={setAnnual}/>}
+      />
+      <Row
+        left="Premium"
+        right={isNaN(premium) ? 'n/a' : `${round_to(100 * premium, 1)}%`}
+      />
+    </div>
   </form>;
 }
