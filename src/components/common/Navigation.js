19319611c9917ae0238fd4434cc39a948c247d4b
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-24 14:34:09 +0200

    Added shading.

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navigation(props) {
   const { items, level } = props;
   const navTheme = level ? `w3-theme-d${5 - level}` : 'w3-theme-d4';
-  const navStyle = `my-nav w3-bar ${navTheme}`;
+  const navStyle = `w3-card my-nav w3-bar ${navTheme}`;
   const linkStyle = 'w3-bar-item w3-button w3-hover-theme';
   const offset = { top: `${40 * (level - 1)}px` };
   return <nav className={navStyle} style={offset}>
--- src/components/common/Panel.js
+++ src/components/common/Panel.js
@@ -5,7 +5,9 @@ export default function Panel(props) {
   const _theme = !theme ? 'w3-theme-d3' : theme;
   const _padding = padding ? 'w3-padding-16' : '';
   const _margin = margin === undefined || margin === true ? 'w3-margin' : '';
-  return <div className={`${_margin} w3-padding-large ${_theme} my-round`}>
+  return <div
+    className={`w3-card ${_margin} w3-padding-large ${_theme} my-round`}
+  >
     {
       title === false ? ''
         : <h4 className="w3-center my-panel-title">{title}</h4>
