35d30d14290193f99b53cb83e1579a67fa421ee7
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-27 16:27:41 +0200

    Drew my own candlestick charts.

--- src/components/puzzle/MovingAverages.js
+++ src/components/puzzle/MovingAverages.js
@@ -1,236 +1,195 @@
 import React from 'react';
 
-import CanvasJSReact from '../../canvasjs.react';
+import { min, max } from 'd3-array';
+import { scaleLinear, scaleUtc } from 'd3-scale';
+import { axisRight, axisBottom } from 'd3-axis';
 
 import { Up, Down, Gold, Death, SplayUp, SplayDown } from '../common/Icons';
 import Indicator from './Indicator';
+import TitledChart from './TitledChart';
+import {
+  appendPathLine,
+  appendForecastRect,
+} from '../../util/svg';
 
-export default function MovingAverages() {
-  return <Indicator
-    options={{
-      limit:   40,
-      columns: [
-        'open', 'high', 'low', 'close',
-        'ema_21', 'ema_55', 'ema_89', 'ema_200', 'ema_377',
-        'sma_10', 'sma_200',
-      ],
-    }}
-    chart={({ title, history, forecast }) => {
-      if (!history) { return null; }
-    
-      const {
-        open, high, low, close,
-        ema_21, ema_55, ema_89, ema_200, ema_377,
-        sma_10, sma_200,
-      } = history;
-    
-      // Resample candles (a bit of overhead, but clean)
-      const ohlc = [];
-      let lowest = Infinity, highest = -Infinity;
-      for (let i = 0; i < open.length; i++) {
-        ohlc.push({ x: open[i].x, y: [
-          open[i].y, high[i].y, low[i].y, close[i].y
-        ]});
-        lowest = Math.min(low[i].y, lowest);
-        highest = Math.max(high[i].y, highest);
-      }
-    
+export default () => <Indicator
+  options={{
+    limit:   40,
+    columns: [
+      'open', 'high', 'low', 'close',
+      'ema_21', 'ema_55', 'ema_89', 'ema_200', 'ema_377',
+      'sma_10', 'sma_200',
+    ],
+  }}
+  chart={({
+    title,
+    history: {
+      open, high, low, close,
+      ema_21, ema_55, ema_89, ema_200, ema_377,
+      sma_10, sma_200,
+    },
+    forecast: {
+      '0': {
+        ema_21:  ema_21_expected,
+        ema_55:  ema_55_expected,
+        ema_89:  ema_89_expected,
+        ema_200: ema_200_expected,
+        ema_377: ema_377_expected,
+        sma_10:  sma_10_expected,
+        sma_200: sma_200_expected,
+      },
+    },
+    width,
+    height,
+    margin = { top: 4, right: 40, bottom: 18, left: 15 },
+  }) => <TitledChart
+    width={width}
+    height={height}
+    title={() => {
+
       // Close crossed above or below the 21 ema
-      const crossed_up = close[0].y > ema_21[0].y;
-      const crossed_down = close[0].y < ema_21[0].y;
-    
-      // 55 ema crossed above or below the 200 ema
-      const golden_cross = ema_55[0].y > ema_200[0].y;
-      const death_cross = ema_55[0].y < ema_200[0].y;
-    
-      // Full splays
-      const splay_bull = ema_21[0].y > ema_55[0].y
-                      && ema_55[0].y > ema_89[0].y
-                      && ema_89[0].y > ema_200[0].y;
-      const splay_bear = ema_21[0].y < ema_55[0].y
-                      && ema_55[0].y < ema_89[0].y
-                      && ema_89[0].y < ema_200[0].y;
-    
-      const options = {
-        animationEnabled: true,
-        dataPointWidth:   3,
-        theme:            "dark2",
-        backgroundColor:  "transparent",
-        height:           180,
-        axisX:            {
-          lineThickness:     0.5,
-          crosshair:         {
-            enabled:         true,
-            snapToDataPoint: true,
-          },
-          stripLines:        [{
-            value:        forecast[0].close[0].x,
-            color:        "white",
-            opacity:      0.5,
-            lineDashType: "dash",
-          }, {
-            startValue:   forecast[0].close[0].x,
-            endValue:     forecast[0].close[forecast[0].close.length - 1].x,
-            color:        "white",
-            opacity:      0.07,
-          }],
-        },
-        axisY:            [{
-          minimum:           lowest,
-          maximum:           highest,
-          valueFormatString: "#.#",
-          gridColor:         "transparent",
-          lineThickness:     0.5,
-          crosshair:         {
-            enabled:         true,
-            labelMaxWidth:   40,
-          },
-        }],
-        data:             [
-    
-        // Expected data
-        {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].ema_21,
-          markerType:    "none",
-          lineColor:     "yellow",
-          lineThickness: 2,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].ema_377,
-          markerType:    "none",
-          color:         "purple",
-          lineThickness: 1,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].ema_200,
-          markerType:    "none",
-          color:         "navy",
-          lineThickness: 3,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].sma_200,
-          markerType:    "none",
-          color:         "white",
-          lineThickness: 1,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].ema_89,
-          markerType:    "none",
-          color:         "cyan",
-          lineThickness: 1,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].ema_55,
-          markerType:    "none",
-          color:         "green",
-          lineThickness: 3,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].ema_21,
-          markerType:    "none",
-          color:         "yellow",
-          lineThickness: 2,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    forecast[0].sma_10,
-          markerType:    "none",
-          color:         "red",
-          lineThickness: 1,
-        }, 
-        
-        // Historical data
-        {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    ema_377,
-          markerType:    "none",
-          color:         "purple",
-          lineThickness: 1,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    ema_200,
-          markerType:    "none",
-          color:         "navy",
-          lineThickness: 3,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    sma_200,
-          markerType:    "none",
-          color:         "white",
-          lineThickness: 1,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    ema_89,
-          markerType:    "none",
-          color:         "cyan",
-          lineThickness: 1,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    ema_55,
-          markerType:    "none",
-          color:         "green",
-          lineThickness: 3,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    ema_21,
-          markerType:    "none",
-          color:         "yellow",
-          lineThickness: 2,
-        }, {
-          type:          "line",
-          xValueType:    "dateTime",
-          dataPoints:    sma_10,
-          markerType:    "none",
-          color:         "red",
-          lineThickness: 1,
-        }, {
-          type:         "candlestick",
-          xValueType:   "dateTime",
-          dataPoints:   ohlc,
-          markerType:   "none",
-          risingColor:  "white",
-          fallingColor: "black",
-          color:        "white",
-          lineThickness: 0,
-        }]
+      let triangle = null;
+      if (close[0].y > ema_21[0].y) {
+        triangle = <Up title="Above 21 EMA"/>;
+      } else if (close[0].y < ema_21[0].y) {
+        triangle = <Down title="Below 21 EMA"/>;
       };
     
-      return <div className="w3-cell my-fourth">
-        {title} {
-          crossed_up
-            ? <Up title="Above 21 EMA"/>
-            : crossed_down
-              ? <Down title="Below 21 EMA"/>
-              : ''
-        } {
-          golden_cross
-            ? <Gold title="Golden cross"/>
-            : death_cross
-              ? <Death title="Death cross"/>
-              : ''
-        } {
-          splay_bull
-            ? <SplayUp title="Full bullish splay"/>
-            : splay_bear
-              ? <SplayDown title="Full bearish splay"/>
-              : ''
-        }
-        <CanvasJSReact.CanvasJSChart options={options}/>
-      </div>;
+      // 55 ema crossed above or below the 200 ema
+      let cross = null;
+      if (ema_55[0].y > ema_200[0].y) {
+        cross = <Gold title="Golden cross"/>;
+      } else if (ema_55[0].y < ema_200[0].y) {
+        cross = <Death title="Death cross"/>;
+      }
+    
+      // Full splays
+      let splay = null;
+      if (ema_21[0].y > ema_55[0].y 
+        && ema_55[0].y > ema_89[0].y
+        && ema_89[0].y > ema_200[0].y) {
+        splay = <SplayUp title="Full bullish splay"/>;
+      } else if (ema_21[0].y < ema_55[0].y
+        && ema_55[0].y < ema_89[0].y
+        && ema_89[0].y < ema_200[0].y) {
+        splay = <SplayDown title="Full bearish splay"/>;
+      }
+
+      return <>{title} {triangle} {cross} {splay}</>;
     }}
-  />;
-}
+    draw={svg => {
+      
+      // X-axis breakpoints
+      const first = ema_21[ema_21.length - 1].x;
+      const middle = ema_21_expected[0].x // not really middle
+      const last = ema_21_expected[ema_21_expected.length - 1].x
+  
+      // X-axis
+      const x = scaleUtc()
+        .domain([first, last])
+        .range([margin.left, width - margin.right]);
+  
+      svg.append('g')
+        .attr('transform', `translate(0,${height - margin.bottom})`)
+        .call(axisBottom(x)
+          .ticks(5)
+          .tickSizeOuter(0));
+
+      // Y-axis
+      const y = scaleLinear()
+        .domain([min(low, d => d.y), max(high, d => d.y)])
+        .range([height - margin.bottom, margin.top]);
+      const [lowest, highest] = y.domain();
+  
+      svg.append('g')
+        .attr('transform', `translate(${width - margin.right},0)`)
+        .call(axisRight(y)
+          .ticks(5)
+          .tickSizeOuter(0));
+  
+      // Mark forecast
+      appendForecastRect(svg)(x(middle), x(last), y(highest), y(lowest));
+
+      // Clip path
+      const clipPathId = `rect-clip-${title}`;
+      svg.append('clipPath')
+        .attr('id', clipPathId)
+        .append('rect')
+          .attr('x', x(first))
+          .attr('y', y(highest))
+          .attr('width', x(last) - x(first))
+          .attr('height', y(lowest) - y(highest));
+
+      // MAs
+      const valid = d => d.y && d.y !== null;
+      const appendMA = data => appendPathLine(svg)(data.filter(valid), x, y)
+        .attr('clip-path', `url(#${clipPathId})`);
+
+      appendMA(sma_10)
+        .attr('stroke', 'red');
+      appendMA(ema_21)
+        .attr('stroke', 'yellow');
+      appendMA(ema_55)
+        .attr('stroke', 'green');
+      appendMA(ema_89)
+        .attr('stroke', 'cyan');
+      appendMA(ema_200)
+        .attr('stroke', 'navy');
+      appendMA(sma_200)
+        .attr('stroke', 'white');
+
+      appendMA(sma_10_expected)
+        .attr('stroke', 'red');
+      appendMA(ema_21_expected)
+        .attr('stroke', 'yellow');
+      appendMA(ema_55_expected)
+        .attr('stroke', 'green');
+      appendMA(ema_89_expected)
+        .attr('stroke', 'cyan');
+      appendMA(ema_200_expected)
+        .attr('stroke', 'navy');
+      appendMA(sma_200_expected)
+        .attr('stroke', 'white');
+
+      // Candles
+      const candles = open.map((open, i) => ({
+        x:     open.x,
+        open:  open.y,
+        high:  high[i].y,
+        low:   low[i].y,
+        close: close[i].y,
+      }));
+
+      // Candle width
+      const candleWidth = (x(middle) - x(first)) / candles.length - 2;
+
+      const candleColor = d => d.close < d.open ? 'black' : 'white';
+      // const candleColor = d => d.close < d.open ? 'red' : 'green';
+      const wickColor = 'white';
+      // const wickColor = candleColor;
+      const g = svg.append('g')
+        .selectAll('g')
+          .data(candles)
+          .join('g')
+            .attr('transform', d => `translate(${x(d.x)},0)`)
+            .attr('stroke', wickColor);
+
+      g.append('line')
+        .attr('y1', d => y(d.low))
+        .attr('y2', d => y(d.high));
+
+      // g.append('line')
+      //   .attr('y1', d => y(d.open))
+      //   .attr('y2', d => y(d.close))
+      //   .attr('stroke-width', candleWidth);
+
+      // Alternative using rectangles: more customizable
+      g.append('rect')
+        .attr('transform', `translate(-${candleWidth / 2},0)`)
+        .attr('y', d => y(Math.max(d.open, d.close)))
+        .attr('width', candleWidth)
+        .attr('height', d => Math.abs(y(d.open) - y(d.close)))
+        .attr('fill', candleColor);
+    }}
+  />}
+/>;
