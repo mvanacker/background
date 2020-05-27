23bb601b146ece4e35918acbae8ccd85b553d8af
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-27 12:11:29 +0200

    Changed order of menu items.

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -3,20 +3,20 @@ import Navigation from './common/Navigation';
 
 export default function Puzzle() {
   return <Navigation level={2} items={[{
-      title: 'Confluence',
-      path:  '/confluence',
+      title: 'Volatility',
+      path:  '/volatility',
     }, {
       title: 'MAs',
       path:  '/moving-averages',
+    }, {
+      title: 'Confluence',
+      path:  '/confluence',
     }, {
       title: 'Stochs',
       path:  '/stochs',
     }, {
       title: 'RSI',
       path:  '/rsi',
-    }, {
-      title: 'Volatility',
-      path:  '/volatility',
     // }, {
     //   title: 'Pattern/Volume/Other symbols',
     //   path:  '/to-do',
