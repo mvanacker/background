d99e88914e6fba14009add5c623ee53188a69a82
Maurits Van Acker <maurits.vanacker@gmail.com>
2020-05-29 19:56:59 +0200

    Added JSON stringifying and parsing.

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -6,9 +6,9 @@ export const set = (id, val) => localStorage.setItem(id, val);
 export default function useStorage(id, initialValue = '') {
   const storedValue = get(id);
   if (storedValue) {
-    initialValue = storedValue;
+    initialValue = JSON.parse(storedValue);
   }
   const [value, setValue] = useState(initialValue);
-  useEffect(() => set(id, value), [id, value]);
+  useEffect(() => set(id, JSON.stringify(value)), [id, value]);
   return [value, setValue];
 }
