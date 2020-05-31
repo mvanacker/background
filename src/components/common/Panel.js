03e69f6102239c433bfee461817944b6e16249f4
mvanacker <maurits.vanacker@gmail.com>
2020-05-31 19:24:55 +0200

    Removed default padding. Prettified.

--- src/components/common/Panel.js
+++ src/components/common/Panel.js
@@ -3,25 +3,25 @@ import React from 'react';
 export default ({
   title = false,
   theme = 'w3-theme-d3',
-  padding = 'w3-padding-16',
+  padding = '',
   margin = 'w3-margin',
   className,
   children,
   ...props
 }) => {
-  return <div 
-    className={
-      `
+  return (
+    <div
+      className={`
       w3-card
       ${margin ? margin : ''}
       w3-padding-large
       ${theme}
       my-round
-      ${className}`
-    }
-    {...props}
-  >
-    {title && <h4 className="w3-center my-panel-title">{title}</h4>}
-    <div className={padding ? padding : ''}>{children}</div>
-  </div>;
+      ${className}`}
+      {...props}
+    >
+      {title && <h4 className="w3-center my-panel-title">{title}</h4>}
+      <div className={padding ? padding : ''}>{children}</div>
+    </div>
+  );
 };
