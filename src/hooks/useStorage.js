26810c0fc53b3f23711663f574f417a0a72aaf12
mvanacker <maurits.vanacker@gmail.com>
2020-06-02 11:44:34 +0200

    Added missing dependency to useEffect.

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,18 +1,22 @@
 import { useState, useEffect } from 'react';
 
+const localFuncs = {
+  get: (id) => localStorage.getItem(id),
+  set: (id, val) => localStorage.setItem(id, val),
+  remove: (id) => localStorage.removeItem(id),
+};
+
 export const useLocal = (id, options) =>
-  useStorage(id, options, {
-    get: (id) => localStorage.getItem(id),
-    set: (id, val) => localStorage.setItem(id, val),
-    remove: (id) => localStorage.removeItem(id),
-  });
+  useStorage(id, options, localFuncs);
+
+const sessionFuncs = {
+  get: (id) => sessionStorage.getItem(id),
+  set: (id, val) => sessionStorage.setItem(id, val),
+  remove: (id) => sessionStorage.removeItem(id),
+};
 
 export const useSession = (id, options) =>
-  useStorage(id, options, {
-    get: (id) => sessionStorage.getItem(id),
-    set: (id, val) => sessionStorage.setItem(id, val),
-    remove: (id) => sessionStorage.removeItem(id),
-  });
+  useStorage(id, options, sessionFuncs);
 
 const useStorage = (
   id,
@@ -35,7 +39,7 @@ const useStorage = (
     if (!stateOnly) {
       set(id, JSON.stringify(value));
     }
-  }, [id, value, stateOnly]);
+  }, [id, value, stateOnly, set]);
 
   // Define function to remove a value
   const removeValue = () => remove(id);
