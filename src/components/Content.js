0deff3f4ee22334fae9fb217262d12756f4d986b
mvanacker <maurits.vanacker@gmail.com>
2020-06-02 11:47:59 +0200

    Correctly hide toggle handle on smaller screens. Prettified.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,47 +1,49 @@
 import React, { useState } from 'react';
-import ReactResizeDetector from 'react-resize-detector'
+import ReactResizeDetector from 'react-resize-detector';
 import { DoubleLeft, DoubleRight } from './common/Icons';
 
 const LAPTOP_WIDTH = 1366 * 0.9;
-const LEFT_WIDTH   = '500px';
+const LEFT_WIDTH = '500px';
 
-export default ({ left, right}) => {
+export default ({ left, right }) => {
   const [visible, setVisible] = useState(true);
   const [enabled, setEnabled] = useState(window.innerWidth >= LAPTOP_WIDTH);
-  return <ReactResizeDetector handleWidth>
-    {
-      ({ width }) => {
+  return (
+    <ReactResizeDetector handleWidth>
+      {({ width }) => {
         setEnabled(width >= LAPTOP_WIDTH);
 
-        return <>
-          <div
-            className="w3-card w3-theme-l1 toggle-handle my-round-right"
-            onClick={() => setVisible(!visible)}
-          >
-            {
-              enabled && visible
-                ? <DoubleLeft title="Hide"/>
-                : <DoubleRight title="Show"/>
-            }
-          </div>
-          <div className="w3-cell-row">
-            {
-              enabled && visible && <div
-                className="w3-cell"
-                style={{
-                  minWidth: LEFT_WIDTH,
-                  width:    LEFT_WIDTH,
-                }}
+        return (
+          <>
+            {enabled && (
+              <div
+                className="w3-card w3-theme-l1 toggle-handle my-round-right"
+                onClick={() => setVisible(!visible)}
               >
-                {left}
+                {visible ? (
+                  <DoubleLeft title="Hide" />
+                ) : (
+                  <DoubleRight title="Show" />
+                )}
               </div>
-            }
-            <div className="w3-cell">
-              {right}
+            )}
+            <div className="w3-cell-row">
+              {enabled && visible && (
+                <div
+                  className="w3-cell"
+                  style={{
+                    minWidth: LEFT_WIDTH,
+                    width: LEFT_WIDTH,
+                  }}
+                >
+                  {left}
+                </div>
+              )}
+              <div className="w3-cell">{right}</div>
             </div>
-          </div>
-        </>
-      }
-    }
-  </ReactResizeDetector>;
+          </>
+        );
+      }}
+    </ReactResizeDetector>
+  );
 };
