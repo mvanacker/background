c559bd421732459b4acab73e67147a443ccf8a14
mvanacker <maurits.vanacker@gmail.com>
2020-06-06 15:04:11 +0200

    Fixed resize detection.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import ReactResizeDetector from 'react-resize-detector';
+import React, { useState, useEffect } from 'react';
 import { useSession } from '../hooks/useStorage';
 import { DoubleLeft, DoubleRight } from './common/Icons';
 
@@ -10,43 +9,40 @@ export default ({ left, right }) => {
   const [visible, setVisible] = useSession('sidebar-visible', {
     initialValue: true,
   });
-  const [enabled, setEnabled] = useState(window.innerWidth >= LAPTOP_WIDTH);
-  return (
-    <ReactResizeDetector handleWidth>
-      {({ width }) => {
-        setEnabled(width >= LAPTOP_WIDTH);
+  
+  // Disable left sidebar on smaller screens
+  const isWindowLarge = () => window.innerWidth >= LAPTOP_WIDTH;
+  const [enabled, setEnabled] = useState(isWindowLarge());
+  useEffect(() => {
+    const callback = () => setEnabled(isWindowLarge());
+    window.addEventListener('resize', callback);
+    return () => window.removeEventListener('resize', callback);
+  });
 
-        return (
-          <>
-            {enabled && (
-              <div
-                className="w3-card w3-theme-l1 toggle-handle my-round-right"
-                onClick={() => setVisible(!visible)}
-              >
-                {visible ? (
-                  <DoubleLeft title="Hide" />
-                ) : (
-                  <DoubleRight title="Show" />
-                )}
-              </div>
-            )}
-            <div className="w3-cell-row">
-              {enabled && visible && (
-                <div
-                  className="w3-cell"
-                  style={{
-                    minWidth: LEFT_WIDTH,
-                    width: LEFT_WIDTH,
-                  }}
-                >
-                  {left}
-                </div>
-              )}
-              <div className="w3-cell">{right}</div>
-            </div>
-          </>
-        );
-      }}
-    </ReactResizeDetector>
+  return (
+    <>
+      {enabled && (
+        <div
+          className="w3-card w3-theme-l1 toggle-handle my-round-right"
+          onClick={() => setVisible(!visible)}
+        >
+          {visible ? <DoubleLeft title="Hide" /> : <DoubleRight title="Show" />}
+        </div>
+      )}
+      <div className="w3-cell-row">
+        {enabled && visible && (
+          <div
+            className="w3-cell"
+            style={{
+              minWidth: LEFT_WIDTH,
+              width: LEFT_WIDTH,
+            }}
+          >
+            {left}
+          </div>
+        )}
+        <div className="w3-cell">{right}</div>
+      </div>
+    </>
   );
 };
