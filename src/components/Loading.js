a17a3532a2fd6377a892de5b1004e52ff67b8d8a
mvanacker <maurits.vanacker@gmail.com>
2020-06-09 19:09:26 +0200

    Replaced table with flex.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,14 +4,15 @@ import { Loading256 } from './common/Icons';
 // 80px menu, 2x16px margins, 2x16px padding
 const defaultOffset = 112;
 
-export default ({ offset }) => <div
-  className="w3-content"
-  style={{
-    display: 'table',
-    height: `calc(100vh - ${offset ? offset : defaultOffset}px)`,
-  }}
->
-  <div className="w3-cell w3-cell-middle">
-    <Loading256/>
+export default ({ offset }) => (
+  <div
+    style={{
+      display: 'flex',
+      height: `calc(100vh - ${offset ? offset : defaultOffset}px)`,
+    }}
+  >
+    <div style={{ margin: 'auto' }}>
+      <Loading256 />
+    </div>
   </div>
-</div>;
+);
