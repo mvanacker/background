258136a749f051d9d147557a81086c1051a7784d
mvanacker <maurits.vanacker@gmail.com>
2020-06-14 15:36:11 +0200

    Removed redundant props.

--- src/components/common/Panel.js
+++ src/components/common/Panel.js
@@ -1,25 +1,14 @@
 import React from 'react';
 
-export default ({
-  title = false,
-  theme = 'w3-theme-d3',
-  margin = 'w3-margin',
-  className,
-  children,
-  ...props
-}) => {
-  return (
-    <div
-      className={`w3-card ${
-        margin ? margin : ''
-      } w3-padding-large ${theme} my-round ${className ? className : ''}`}
-      {...props}
-    >
-      {title && <PanelTitle>{title}</PanelTitle>}
-      {children}
-    </div>
-  );
-};
+export default ({ title = false, className = '', children, ...props }) => (
+  <div
+    className={`w3-card w3-margin w3-padding-large w3-theme-d3 my-round ${className}`}
+    {...props}
+  >
+    {title && <PanelTitle>{title}</PanelTitle>}
+    {children}
+  </div>
+);
 
 export const PanelTitle = ({ children, className = '', ...props }) => (
   <h4 className={`w3-center my-panel-title ${className}`} {...props}>
