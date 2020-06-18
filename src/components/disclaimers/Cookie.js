db4bd7e418c9050615df1b5b0839859edbc16c42
mvanacker <maurits.vanacker@gmail.com>
2020-06-18 11:36:29 +0200

    Edited.

--- src/components/disclaimers/Cookie.js
+++ src/components/disclaimers/Cookie.js
@@ -11,16 +11,17 @@ export default () => {
         <div className="w3-content w3-card w3-theme-l1 w3-padding-large w3-center my-round-top w3-border my-cookie-disclaimer">
           <p>
             This website stores your preferences, notes, marked checkboxes and
-            so on in your <b>local storage</b>. It's similar to using cookies,
-            but neither are great. This application's creator allowed use of
-            this application to store information on your device with the sole
-            purpose of customizing this application and nothing else.
+            other customizations in your browser's <b>local storage</b>. It's
+            similar to using cookies, but neither are flawless. This
+            application's creator allows this application, if used, to store
+            information on your device with the sole purpose of customizing this
+            application and nothing else.
           </p>
           <button
             className="w3-btn w3-theme-d1 my-round w3-border"
             onClick={() => setHide(true)}
           >
-            Okay!
+            Okay
           </button>
         </div>
       </div>
