20a2a5a97325e3d5cdc1da41c803403bee8c82c3
mvanacker <maurits.vanacker@gmail.com>
2020-06-19 18:21:35 +0200

    Added About page.

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Panel from './common/Panel';
+
+const Bubble = ({ children }) => (
+  <Panel title={false} className="my-bubble">
+    {children}
+  </Panel>
+);
+
+export default () => (
+  <>
+    <Bubble>Hello! My name is Maurits.</Bubble>
+    <Bubble>I like coding and I like trading.</Bubble>
+    <Bubble>Through this app I share a piece of my heart and soul.</Bubble>
+  </>
+);
