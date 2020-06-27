194e1d80176f8eea71ec294eedab95ef66d5f90a
mvanacker <maurits.vanacker@gmail.com>
2020-06-27 11:09:34 +0200

    Attempting null pointer fix.

--- src/util/d3-axis-util.js
+++ src/util/d3-axis-util.js
@@ -4,12 +4,16 @@ const bb = (selection) => selection.node().getBoundingClientRect();
 
 export const removeOverlapWithDatum = (selection, datum) => {
   const ticks = selection.selectAll('g.tick');
-  const { x: x1, width } = bb(ticks.filter((d) => d === datum));
+  const datumSelection = ticks.filter((d) => d === datum);
+  if (!datumSelection.empty()) return;
+  const { x: x1, width } = bb(datumSelection);
   const x2 = x1 + width;
   ticks
     .filter((d, i, g) => {
       if (d === datum) return false;
-      const { x: t1, width: textWidth } = bb(select(g[i]).selectAll('text'));
+      const nextText = select(g[i]).selectAll('text');
+      if (!nextText.empty()) return false;
+      const { x: t1, width: textWidth } = bb(nextText);
       const t2 = t1 + textWidth;
       return (t1 <= x1 && x1 <= t2) || (t1 <= x2 && x2 <= t2);
     })
@@ -28,12 +32,14 @@ export const removeOverlap = (selection) => {
       let done = false;
       while (g[i + k] && !done) {
         const nextText = select(g[i + k]).selectAll('text');
-        const overlapped = bb(nextText).x < x2;
-        if (overlapped) {
-          nextText.remove();
+        if (!nextText.empty()) {
+          const overlapped = bb(nextText).x < x2;
+          if (overlapped) {
+            nextText.remove();
+          }
+          done = !overlapped;
+          k++;
         }
-        done = !overlapped;
-        k++;
       }
     }
   });
