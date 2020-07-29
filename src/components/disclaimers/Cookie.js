89815d7d89b35fcea69d73900fab5238e27385ef
mvanacker <maurits.vanacker@gmail.com>
2020-07-29 21:33:33 +0200

    Simplified.

--- src/components/disclaimers/Cookie.js
+++ src/components/disclaimers/Cookie.js
@@ -10,19 +10,16 @@ export default () => {
       <div className="my-cookie-disclaimer-container">
         <div className="w3-content w3-card w3-theme-l1 w3-padding-large w3-center my-round-top w3-border my-cookie-disclaimer">
           <p>
-            This website stores your preferences, notes, marked checkboxes and
-            other customizations in your browser's{' '}
-            <a href="https://w3.org/TR/webstorage">storage</a>. It's similar to
-            using cookies, but neither are flawless. This application's creator
-            allows this application, if used, to store information on your
-            device with the sole purpose of customizing this application and
-            nothing else.
+            This website uses{' '}
+            <a href="https://w3.org/TR/webstorage">web storage</a> to save your
+            preferences on your computer. This website does absolutely{' '}
+            <b>no tracking</b> of any kind.
           </p>
           <button
             className="w3-btn w3-theme-d1 my-round w3-border"
             onClick={() => setHide(true)}
           >
-            Okay
+            I accept
           </button>
         </div>
       </div>
