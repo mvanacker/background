941aa442464f5550f9a0aa2607ff5c495eed8f10
mvanacker <maurits.vanacker@gmail.com>
2020-07-29 21:33:49 +0200

    Fixed centering.

--- src/components/disclaimers/Financial.js
+++ src/components/disclaimers/Financial.js
@@ -16,48 +16,46 @@ export default ({ children }) => {
     children
   ) : (
     <div className="my-financial-disclaimer-container">
-      <div>
-        <Panel
-          title="Disclaimer"
-          className="w3-center w3-mobile w3-content my-financial-disclaimer"
-        >
-          <div className="w3-padding-large">
-            <p>
-              Information on this website is not financial advice, nor should it
-              be treated as a substitute for the services of a certified
-              financial advisor.
-            </p>
-            <p>
-              You are responsible for your own actions or lack thereof. You have
-              the power to make up your own mind.
-            </p>
-          </div>
-          <div className="w3-padding-large">
-            <button
-              className="w3-btn w3-theme-l2 w3-margin my-round"
-              onClick={() => {
-                setAgreed(true);
-                setAgreedOnce(true);
-              }}
-            >
-              I agree
-            </button>
-            {agreedOnce && (
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    className="my-check"
-                    checked={remember}
-                    onChange={(e) => setRemember(e.target.checked)}
-                  />{' '}
-                  Remember
-                </label>
-              </div>
-            )}
-          </div>
-        </Panel>
-      </div>
+      <Panel
+        title="Disclaimer"
+        className="w3-center w3-mobile w3-content my-financial-disclaimer"
+      >
+        <div className="w3-padding-large">
+          <p>
+            Information on this website is not financial advice, nor should it
+            be treated as a substitute for the services of a certified financial
+            advisor.
+          </p>
+          <p>
+            You are responsible for your own actions or lack thereof. You have
+            the power to make up your own mind.
+          </p>
+        </div>
+        <div className="w3-padding-large">
+          <button
+            className="w3-btn w3-theme-l2 w3-margin my-round"
+            onClick={() => {
+              setAgreed(true);
+              setAgreedOnce(true);
+            }}
+          >
+            I agree
+          </button>
+          {agreedOnce && (
+            <div>
+              <label>
+                <input
+                  type="checkbox"
+                  className="my-check"
+                  checked={remember}
+                  onChange={(e) => setRemember(e.target.checked)}
+                />{' '}
+                Remember
+              </label>
+            </div>
+          )}
+        </div>
+      </Panel>
     </div>
   );
 };
